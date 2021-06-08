c9dab1fd0abd3e834d7c2cf852ccd8ca9f630b6d
Floor Buschenhenke <woordheks@gmail.com>
2021-06-08 12:44:06 +0200

    added rugplot for action scatter @emilia chrononumber5

--- scripts/actionplots_duration_bar.R
+++ scripts/actionplots_duration_bar.R
@@ -9,7 +9,7 @@ names(grotetabelzonderoutliers)
 ## dit is nog met slechte duur voor focus (en weggefilterde mouseevents daarbinnen)
 
 testje <- grotetabelzonderoutliers %>%
-  filter(participant == "De Hemel", session_number == 2)
+  filter(participant == "Emilia", chrononumber == 5)
 
 ggplot(testje, aes(jump_number, jump_duration, fill = event_type))+ geom_col()
 
@@ -19,7 +19,10 @@ options(scipen = "999")
 ggplot(testje, aes(jump_number, jump_duration, colour = event_type))+ 
   geom_jitter()+
    geom_rug( position = "jitter", size = 0.9, length = unit(0.1, "npc"), outside = T )+
-  coord_cartesian(clip = "off")+
+  coord_cartesian(clip = "off")+ theme(
+    axis.text.x = element_blank())+
+  labs(title = "Emilia's process, 5th session", subtitle = "events are ordered chronologically along the x-axis",
+       x = "")+
   scale_color_fira()
  
 #geom_point() 
